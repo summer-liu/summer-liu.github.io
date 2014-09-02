e31f2645f9f4b929269f090829b163e2e160daec
Tao <liutao.summer@gmail.com>
2014-09-02 11:03:29 +0800

    add files

---
+++ oneway.js
@@ -0,0 +1,19 @@
+
+var w = 700, h = 500;
+var projection = d3.geo.albersUsa()
+					.translate([w/2, h/2])
+					.scale([500]);
+
+var path = d3.geo.path()
+				.projection(projection);
+
+var svg = d3.select("body").append("svg");
+
+d3.json("gz_2010_us_040_00_20m.json", function(json){
+	svg.selectAll("path")
+		.data(json.features)
+		.enter()
+		.append("path")
+		.attr("d",path)
+})
+
