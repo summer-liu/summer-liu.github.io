bb4ec68f73b679a8bd316edebfd9fd835ae4cdad
Tao <liutao.summer@gmail.com>
2015-09-15 21:52:53 +0800

    update

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Tao on 15/9/15.
+ */
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#myMenu',
+        lockAnchors: true,
+        anchors:['Home', 'Skills','Experience', 'Projects','Education','Contact'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Home', 'Skills','Experience', 'Projects','Education','Contact'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: true,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : false,
+        sectionsColor : ['#4BBFC3','#1bbc9b', '#7BAABE', 'whitesmoke', '#ccddff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
